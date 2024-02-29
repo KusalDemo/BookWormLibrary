fff527444b468604f37520218cb6513bfac037d1
KusalDemo <kusaldemo2022@gmail.com>
2024-02-29 06:10:21 +0530

    Updated signup_form

---
+++ src/main/java/org/example/controller/SignUpFormController.java
@@ -0,0 +1,22 @@
+package org.example.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignUpFormController {
+
+    public TextField txtUserName;
+    public PasswordField txtRePassword;
+    public PasswordField txtPassword;
+    public Button btnAlreadyHaveAnAccount;
+    public Button btnSignUp;
+    public TextField txtEmail;
+
+    public void btnSignUpOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAlreadyHaveAnAccountOnAction(ActionEvent actionEvent) {
+    }
+}
